b8d1b8cf13b77577c9942e9da71518c9733bc133
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2022-02-09 13:34:01 +0100

    Add files via upload

---
+++ Gyakorlatok/MrHumanRebel/elso_gyak_ora_3.cpp
@@ -0,0 +1,14 @@
+// 1/2 => Konstansok/Literálok
+#include<iostream>
+
+#define NK 42
+#define FJ '!'
+#define SZ "\n\t Az élet értelme: "
+
+using namespace std;
+
+int main() {
+  //  cout << "\n\t Az élet értelme: 42!\n" << endl; // Literálok
+	 cout << SZ << NK << FJ  << "\n" << endl;
+return 0;
+}
