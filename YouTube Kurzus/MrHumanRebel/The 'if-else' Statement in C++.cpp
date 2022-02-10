1512fdf2879b0c15f3ac8467f841299b20ed4f0e
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2022-02-10 07:08:39 -0800

    Create The 'if-else' Statement in C++.cpp

---
+++ YouTube Kurzus/MrHumanRebel/The 'if-else' Statement in C++.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  int num = 0;
+  std::cout << "Adjon meg egy számot!" << std::endl;
+  std::cin >> num;
+  if (num > 10) {
+    std::cout << "A szám nagyobb mint 10." << std::endl;
+}
+  return 0;
+}
