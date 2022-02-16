2d8caceb624da88d4805671ba5ea23e94a92280b
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2022-02-16 11:17:41 -0800

    Create beker_ketszam_ellenorzessel_szamok_kiirni_kozottuk.cpp

---
+++ gyakorlo_feladatok/elso_het/beker_ketszam_ellenorzessel_szamok_kiirni_kozottuk.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Adjon meg egy számot!" << endl;
+  int N,M = 0;
+  bool beker, bekerketto;
+  beker = true;
+  bekerketto = true;
+  while (beker){
+    cin >> N;
+    if (N>1)
+      beker = false;
+    else
+    cout << "Adjon meg egy normális számot!" << endl;
+  }
+  cout << "Adjon meg egy másik számot, ami nagyobb mint az előző!" << endl;
+  while (bekerketto){
+    cin >> M;
+    if (M>1 && M>N)
+      bekerketto = false;
+    else
+    cout << "Adjon meg egy normális számot!" << endl;
+  }
+  cout << "A számok N és M között "<< endl;
+  for (int i=N; i<=M ;i++){
+    cout << i << endl;
+  }  
+  return 0;
+}
