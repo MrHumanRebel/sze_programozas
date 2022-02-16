58750b6234960116581b655a097e78999e47feb2
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2022-02-16 10:44:13 -0800

    Create szambe_ketszeres.cpp

---
+++ gyakorlo_feladatok/elso_het/szambe_ketszeres.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int szam = 0;
+  cout << "Adjon meg egy számot!" << endl;
+  cin >> szam;
+  cout << "A szám kétszerese: " << szam*2 << endl;
+  return 0;
+}
