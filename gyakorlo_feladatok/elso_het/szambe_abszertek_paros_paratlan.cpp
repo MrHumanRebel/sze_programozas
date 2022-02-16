bee2ccbbf35d53df1c8531ed2d07f1a3fe07d25c
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2022-02-16 10:53:11 -0800

    Create szambe_abszertek_paros_paratlan.cpp

---
+++ gyakorlo_feladatok/elso_het/szambe_abszertek_paros_paratlan.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int szam, masikszam, szorzat, extra, osszeg = 0;
+  cout << "Adjon meg egy számot!" << endl;
+  cin >> szam;
+  cout << "A szám abszolút értéke: " << abs(szam) << endl;
+  if (szam % 2 == 0)
+    cout << "A megadott páros szám fele: " << szam/2 << endl;
+  else
+    cout << "A megadott szám 2-vel való osztásának eredménye: " << szam/2 << endl;
+    return 0;
+}
