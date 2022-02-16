213d368a1dd54bc9e7afe05aaba380420a7b9705
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2022-02-16 11:52:15 -0800

    Create primszam.cpp

---
+++ gyakorlo_feladatok/elso_het/primszam.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int szam = 0;
+cout<< "Adjon meg egy számot!"<<endl;
+cin>>szam;
+if (szam % 1 == 0 && szam/szam == 1 && szam % 5 != 0 && szam % 3 != 0 && szam % 10 != 0)
+  cout << "A megadott szám prímszám!"<<endl;  
+return 0;
+}
