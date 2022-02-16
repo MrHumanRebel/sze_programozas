b61ec9cef9b399ebe164c7bda3f6f337e5134fd9
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2022-02-16 10:47:58 -0800

    Create ketszam_szorzat_osszeg_kulonbseg.cpp

---
+++ gyakorlo_feladatok/elso_het/ketszam_szorzat_osszeg_kulonbseg.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int szam, masikszam, szorzat, extra, osszeg = 0;
+  cout << "Adjon meg egy számot!" << endl;
+  cin >> szam;
+  cout << "Adjon meg egy másiki számot!" << endl;
+  cin >> masikszam;
+  osszeg = szam+masikszam;
+  szorzat = szam*masikszam;
+  extra = szorzat-osszeg;
+  cout << "A számok összege: " << osszeg << endl;
+  cout << "A számok szorzata: " << szorzat << endl;
+  cout << "A számok szorzatának és összegének különbsége: " << extra << endl;
+  return 0;
+}
