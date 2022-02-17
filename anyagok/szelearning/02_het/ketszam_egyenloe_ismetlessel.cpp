31d38eef107b24e890d920559c76416028514a63
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2022-02-17 10:38:48 +0100

    Create ketszam_egyenloe_ismetlessel.cpp

---
+++ anyagok/szelearning/02_het/ketszam_egyenloe_ismetlessel.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int szam, masikszam;
+  bool beker;
+  beker = true;
+  cout <<"Adjon meg egy számot"<<endl;
+  cin >> szam;
+  do{
+    cout <<"Adjon meg egy számot"<<endl;
+    cin >> masikszam;
+    if (szam == masikszam){
+      beker = false;
+      cout <<"A két szám egyenlő!"<<endl;
+    }
+    else
+    cout <<"A két szám nem egyenlő!"<<endl;    
+  }while (beker == true);
+  return 0;
+}
