31cb1660fd19c195f932131518ad95c2837c56ff
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2022-02-17 05:20:01 -0800

    Update negyzet_rajzolás_V2.cpp

--- anyagok/szelearning/02_het/negyzet_rajzolás_V2.cpp
+++ anyagok/szelearning/02_het/negyzet_rajzolás_V2.cpp
@@ -5,11 +5,11 @@ int main() {
   int szam;
   cout<<"Adjon meg egy számot, amekkora négyzetet szeretne pritelni!!!"<<endl;
   cin>>szam;
-for(int i=1;szam;i++)
-  cout<<"*";
-for(int i=1;szam-2;i++)
-  cout<<"*"<<"\t*szam"<<"*";
-for(int i=1;szam;i++)
-  cout<<"*";
+  for(int i=1;i<=szam;i++)
+    cout<<"*";
+  for(int i=1;i<=szam-2;i++)
+    cout<<"*"<<"\t*szam"<<"*";
+  for(int i=1;i<=szam;i++)
+    cout<<"*";
   return 0;
 }
