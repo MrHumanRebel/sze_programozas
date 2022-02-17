c62ee3311084a0c5558e614ec7fc3deaf0178491
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2022-02-17 05:58:02 -0800

    Create negyzet_rajzolás_atloval.cpp

---
+++ anyagok/szelearning/02_het/negyzet_rajzolás_atloval.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int szam;
+  cout<<"Adjon meg egy számot, amekkora négyzetet szeretne pritelni!!!"<<endl;
+  cin>>szam;
+  //felső sor print
+  for(int i=1;i<=szam;i++){
+    cout<<"*";
+    cout<<"\t";
+  }
+
+  //sor ugrás
+  cout<<"\n";
+
+  //közepe print
+  for(int i=1;i<=szam-2;i++){
+    cout<<"*";
+    for(int j=1;j<=szam-1;j++){
+      cout<<"\t";
+
+      //egyik átló
+      
+      if (i==j){
+        cout<<"*";
+      }
+
+      //másik átló BETA
+
+      /*for(int k=szam;k>=1;k--){
+        if (k==j){
+        cout<<"*";
+        }
+      }
+      */
+    }
+    cout<<"*";
+    cout<<"\n";
+  }
+
+  //alsó sor print
+
+  for(int i=1;i<=szam;i++){
+    cout<<"*";
+    cout<<"\t";
+  }
+  return 0;
+}
