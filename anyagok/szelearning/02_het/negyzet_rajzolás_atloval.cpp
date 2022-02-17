f1342febbeeb745db76f51396d68011b9e5cfa62
Károly Szakály <karoly.szakaly2000@gmail.com>
2022-02-17 16:11:04 +0100

    feat(szelearning): implement cross axis func to the square drawing script

--- anyagok/szelearning/02_het/negyzet_rajzolás_atloval.cpp
+++ anyagok/szelearning/02_het/negyzet_rajzolás_atloval.cpp
@@ -1,49 +1,53 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
   int szam;
-  cout<<"Adjon meg egy számot, amekkora négyzetet szeretne pritelni!!!"<<endl;
-  cin>>szam;
-  //felső sor print
-  for(int i=1;i<=szam;i++){
-    cout<<"*";
-    cout<<"\t";
+  cout << "Adjon meg egy számot, amekkora négyzetet szeretne pritelni!!!" << endl;
+  cin >> szam;
+  // felső sor print
+  for (int i = 1; i <= szam; i++)
+  {
+    cout << "*";
+    cout << "\t";
   }
 
-  //sor ugrás
-  cout<<"\n\n\n";
+  // sor ugrás
+  cout << "\n\n\n";
 
-  //közepe print
-  for(int i=1;i<=szam-2;i++){
-    cout<<"*";
-    for(int j=1;j<=szam-1;j++){
-      cout<<"\t";
+  int keresztAtlo = szam - 2;
+  // közepe print
+  for (int i = 1; i <= szam - 2; i++)
+  {
+    cout << "*";
+    for (int j = 1; j <= szam - 1; j++)
+    {
+      cout << "\t";
 
-      //egyik átló
-      
-      if (i==j){
-        cout<<"*";
+      // egyik átló
+      if (j == i)
+      {
+        cout << "*";
       }
-
-      //másik átló BETA
-
-      /*for(int k=szam;k>=1;k--){
-        if (k==j){
-        cout<<"*";
-        }
+      else if (j == keresztAtlo) // másik átló
+      {
+        cout << "*";
       }
-      */
     }
-    cout<<"*";
-    cout<<"\n\n\n";
+
+    keresztAtlo--;
+
+    cout << "*";
+    cout << "\n\n\n";
   }
 
-  //alsó sor print
-
-  for(int i=1;i<=szam;i++){
-    cout<<"*";
-    cout<<"\t";
+  // alsó sor print
+  for (int i = 1; i <= szam; i++)
+  {
+    cout << "*";
+    cout << "\t";
   }
+
   return 0;
 }
