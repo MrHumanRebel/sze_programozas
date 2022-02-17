a1e4155c1974e6c111cba8fb8bc635fb0e9590f1
MrHumanRebel <szekelydani5g@gmail.com>
2022-02-17 16:31:06 +0100

    Lepcso Rajzolas PLZ

---
+++ anyagok/szelearning/02_het/lepcso_rajzolas.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int szam;
+  cout << "Adja meg a lépcső magasságát!"<<endl;
+  cin >> szam;
+  for (int i=1;i<=szam;i++){
+      cout<<"*";
+      if(i>1){
+        for(int j=1;j<=i-1;j++){          
+        cout<<"*";          
+        }
+      }
+      cout<<"\n";      
+  }
+  return 0;
+}
