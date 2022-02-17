1e68f55d64420cfa10580230a363b2cd13af3021
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2022-02-17 10:35:48 +0100

    Create beker_ameddig_paratlan_parosfele.cpp

---
+++ anyagok/szelearning/02_het/beker_ameddig_paratlan_parosfele.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int szam;
+  bool beker;
+  beker = true;
+  do{
+    cout <<"Adjon meg egy számot"<<endl;
+    cin >> szam;
+    if (szam % 2 == 0)
+      cout << szam/2<<endl;
+    else
+      beker = false;
+  }while (beker == true);
+  return 0;
+}
