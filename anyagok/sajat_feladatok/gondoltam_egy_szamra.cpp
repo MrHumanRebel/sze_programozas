963e251341f88db0141df2e455b99ba1ec8d1cf9
MrHumanRebel <szekelydani5g@gmail.com>
2022-02-19 11:43:39 +0100

    new game

--- anyagok/sajat_feladatok/gondoltam_egy_szamra.cpp
+++ anyagok/sajat_feladatok/gondoltam_egy_szamra.cpp
@@ -5,20 +5,32 @@ int main()
 {
     int szam, beker, x = 0;
     bool ok = false;
-    cout << "Adja meg a számtartományt!" << endl;
-    cin >> x;
-    szam = rand() % x + 1;
-    cout << "Gondoltam egy számra...\t Találja ki ezt a számot!" << endl;
-    while (!ok)
+    bool jatszik = true;
+    do
     {
-        cin >> beker;
-        if (beker == szam)
+        cout << "Adja meg a számtartományt!" << endl;
+        cin >> x;
+        szam = rand() % x + 1;
+        cout << "Gondoltam egy számra...\t Találja ki ezt a számot!" << endl;
+        while (!ok)
         {
-            cout << "Ön sikeresen kitalálta a számot amire gondoltam!\n Program vége... " << endl;
-            ok = true;
+            cin >> beker;
+            if (beker == szam)
+            {
+                cout << "Ön sikeresen kitalálta a számot amire gondoltam!\n Program vége... " << endl;
+                ok = true;
+            }
+            else
+                cout << "Nem erre a számra gondoltam!\n Próbálja újra! " << endl;
         }
+        cout << "Szeretné folytatni a játékot?\n 1-es gomb => IGEN \n 2-es gomb => NEM" << endl;
+        cin >> beker;
+        if (beker == 1)
+            jatszik = true;
+        if (beker == 2)
+            jatszik = false;
         else
-            cout << "Nem erre a számra gondoltam!\n Próbálja újra! " << endl;
-    }
+            cout << "Nem valós menüpont!" << endl;
+    } while (jatszik);
     return 0;
 }
