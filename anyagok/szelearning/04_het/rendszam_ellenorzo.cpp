9423ae3ff1f154e353022ba0d6f68268b92839a0
MrHumanRebel <szekelydani5g@gmail.com>
2022-02-20 12:33:40 +0100

    beta rendszam alap

---
+++ anyagok/szelearning/04_het/rendszam_ellenorzo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char rszam[7];
+    bool beker=true;
+    bool ok = false;
+
+    do{
+        cout <<"Adjon meg egy magyar rendszámot!"<<endl;
+        cin >> rszam;
+
+
+    } while (beker);
+    return 0;
+    
+}
