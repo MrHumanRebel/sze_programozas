caae77268d6fc1acba1a6bf73947a97e456df40d
MrHumanRebel <szekelydani5g@gmail.com>
2022-02-21 15:02:36 +0100

    add missing -- 2.het gyakorlat

--- gyakorlatok/02_het/02_4_do_while_ciklus.cpp
+++ gyakorlatok/02_het/02_4_do_while_ciklus.cpp
@@ -12,12 +12,12 @@ int main()
 	{
 		szam++;
 		cout << "\n Kérem a(z) " << szam << ". karaktert: ";
-		cin >> kar;
+		// cin >> kar;
 		// cin >> kar  >> kar >> kar; // 3 kar esete pl.: b C d, szóköz!
-		// kar=cin.get();
+		kar = cin.get();
 		cout << "A megadott karakter a(z): " << kar << endl;
-	} while (kar != 'a' && kar != 'A' && kar != 'z' && kar != 'Z');
-	// while(kar!='\n'); // elso masodik
+	} //while (kar != 'a' && kar != 'A' && kar != 'z' && kar != 'Z');
+	while(kar!='\n'); // elso masodik
 	cout << "\n Az Ön kilépő karaktere a(z): " << kar
 		 << " amely a(z) " << szam << "." << endl;
 	return 0;
