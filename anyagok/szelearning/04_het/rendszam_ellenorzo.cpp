86a564e4865de4ddb63af72f7c0d89ca18a7978e
MrHumanRebel <szekelydani5g@gmail.com>
2022-02-21 14:36:55 +0100

    format

--- anyagok/szelearning/04_het/rendszam_ellenorzo.cpp
+++ anyagok/szelearning/04_het/rendszam_ellenorzo.cpp
@@ -1,80 +1,83 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     string rszam;
     bool ok = false;
-    do{
-        cout <<"Adjon meg egy magyar rendszámot!"<<endl;
+    do
+    {
+        cout << "Adjon meg egy magyar rendszámot!" << endl;
         cin >> rszam;
         int vizsgal = 0;
 
-        //elválasztásokkal
-        if(rszam.length() == 7)
+        // elválasztásokkal
+        if (rszam.length() == 7)
         {
-        for(int i=0;i<=2; i++){
-            if (rszam[i] >= 65 && rszam[i] <= 90)
-                vizsgal++;            
-        }
+            for (int i = 0; i <= 2; i++)
+            {
+                if (rszam[i] >= 65 && rszam[i] <= 90)
+                    vizsgal++;
+            }
 
-        for(int j=4;j<=6; j++){
-            if (rszam[j] >= 48 && rszam[j] <= 57)
-                vizsgal++;            
-        }
+            for (int j = 4; j <= 6; j++)
+            {
+                if (rszam[j] >= 48 && rszam[j] <= 57)
+                    vizsgal++;
+            }
 
-        if (rszam[3] == 45)
+            if (rszam[3] == 45)
             {
                 vizsgal++;
-            }  
+            }
 
-        if (rszam[3] == 32)
+            if (rszam[3] == 32)
             {
                 vizsgal++;
-            }  
-
-        if (vizsgal == 7)
-            {
-                ok =true;
-                cout << "A megadott rendszám helyes!"<<endl;
             }
-        else
-            {cout <<"A megadott rendszám nem helyes!"<<endl;
-            vizsgal = 0;ok =false;}
 
+            if (vizsgal == 7)
+            {
+                ok = true;
+                cout << "A megadott rendszám helyes!" << endl;
+            }
+            else
+            {
+                cout << "A megadott rendszám nem helyes!" << endl;
+                vizsgal = 0;
+                ok = false;
+            }
         }
 
-        if(rszam.length() == 6)
+        if (rszam.length() == 6)
         {
-        for(int i=0;i<=2; i++){
-            if (rszam[i] >= 65 && rszam[i] <= 90)
-                vizsgal++;            
-        }
-
-        for(int j=3;j<=5; j++){
-            if (rszam[j] >= 48 && rszam[j] <= 57)
-                vizsgal++;            
-        }
-
-        //elválasztások nélkül
-        if (vizsgal == 6)
+            for (int i = 0; i <= 2; i++)
             {
-                ok =true;
-                cout << "A megadott rendszám helyes!"<<endl;
+                if (rszam[i] >= 65 && rszam[i] <= 90)
+                    vizsgal++;
             }
-        else
-            {cout <<"A megadott rendszám nem helyes!"<<endl;
-            vizsgal = 0;ok =false;}
 
+            for (int j = 3; j <= 5; j++)
+            {
+                if (rszam[j] >= 48 && rszam[j] <= 57)
+                    vizsgal++;
+            }
+
+            // elválasztások nélkül
+            if (vizsgal == 6)
+            {
+                ok = true;
+                cout << "A megadott rendszám helyes!" << endl;
+            }
+            else
+            {
+                cout << "A megadott rendszám nem helyes!" << endl;
+                vizsgal = 0;
+                ok = false;
+            }
         }
 
-
-        
-        
-
-
-    } while (ok==false);
-
+    } while (ok == false);
 
     return 0;
-    
 }
