0853c8db9d32663f066a4d1167dff107dce450d1
MrHumanRebel <szekelydani5g@gmail.com>
2022-02-22 15:33:35 +0100

    im stupid

--- anyagok/sajat_feladatok/ot_szambe_pluszbeker_jobbrashift.cpp
+++ anyagok/sajat_feladatok/ot_szambe_pluszbeker_jobbrashift.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
+#define db 5
 
 int main()
 {
-    int db = 5;
     double szamok[db * db];
     int pluszdb = db;
-    double akt, id;
+    double akt;
     cout << "Adjon meg " << db << " db számot!" << endl;
     for (int k = 1; k <= db; k++)
     {
@@ -18,28 +18,29 @@ int main()
     {
         cout << "\nAdja meg a számot amit be szeretne illeszteni!" << endl;
         cin >> akt;
-        if (akt >= 0)
+        // if (akt >= 0)
+        //{
+        pluszdb++;
+        for (int i = pluszdb; i > 1; i--)
         {
-            pluszdb++;
-            for (int i = pluszdb; i >= 1; i--)
-            {
-                szamok[i + 1] = szamok[i];
-            }
-            szamok[1] = akt;
+            szamok[i + 1] = szamok[i];
         }
+        szamok[0] = akt;
+        //}
 
-        else // BETA VERSION NOT WORKING
+        /*else // BETA VERSION NOT WORKING
         {
             pluszdb++;
-            for (int i = 2; i <= pluszdb; i++)
+            for (int i = pluszdb; i > 1; i--)
             {
                 szamok[i - 1] = szamok[i];
             }
-            szamok[0] = akt;
+            szamok[db] = akt;
         }
+        */
 
         //ÚJ ADATOK KIÍRÁSA
-        for (int j = 1; j <= pluszdb; j++)
+        for (int j = 0; j <= pluszdb; j++)
         {
             cout << szamok[j] << "\t";
         }
