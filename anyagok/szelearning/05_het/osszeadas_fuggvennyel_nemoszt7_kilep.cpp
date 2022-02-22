80723072c4a5188beaadd9944e8a5e456fc926a6
MrHumanRebel <szekelydani5g@gmail.com>
2022-02-22 20:11:16 +0100

    new task

--- anyagok/szelearning/05_het/osszeadas_fuggvennyel_nemoszt7_kilep.cpp
+++ anyagok/szelearning/05_het/osszeadas_fuggvennyel_nemoszt7_kilep.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 using namespace std;
+static int a, b, sum;
+
+int summa()
+{
+    sum = a + b;
+    return sum;
+}
 
 int main()
 {
-    int elsoszam, masikszam, osszeg;
+    int osszeg;
     bool ok = true;
     do
     {
         cout << "Adja meg a számot amihez hozzá szeretne adni!" << endl;
-        cin >> elsoszam;
+        cin >> a;
         cout << "Adja meg a számot amit hozzá szeretne adni az előzőhöz!" << endl;
-        cin >> masikszam;
-        // itt lesz a függvény hívás majd
+        cin >> b;
+        osszeg = summa();
+        cout << "A két szám összege: " << osszeg << endl;
         if (osszeg % 7 != 0)
         {
             ok = false;
