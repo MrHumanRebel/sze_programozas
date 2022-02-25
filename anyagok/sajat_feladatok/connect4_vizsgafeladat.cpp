6a404f6f71e378d2028d9ffd434f8f0eaeab6059
MrHumanRebel <szekelydani5g@gmail.com>
2022-02-25 09:10:40 +0100

    new fg

--- anyagok/sajat_feladatok/connect4_vizsgafeladat.cpp
+++ anyagok/sajat_feladatok/connect4_vizsgafeladat.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 #define SOR 6
-#define OSZL 6
+#define OSZL 7
 char tabla[SOR][OSZL];
 bool van_e_palya = false;
 bool oszl_ok = false;
@@ -47,10 +47,40 @@ int beker()
     }
 }
 
+int allas()
+{
+    cout << "  \t";
+    for (int i = 65; i < (65 + OSZL); i++) // 65 => "A" betű ASCII
+    {
+        cout << char(i);
+    }
+    cout << endl;
+
+    for (int i = 0; i < SOR; i++)
+    {
+        if (i <= SOR - 1)
+            cout << '0' << i + 1 << "\t";
+        else
+            cout << i + 1 << "\t";
+        for (int j = 0; j < OSZL; j++)
+        {
+            cout << tabla[i][j];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
 int main()
 {
     if (van_e_palya == false)
         alappalya();
+    cout << "Első játékos lépése:" << endl;
     beker();
+    allas();
+    cout << "Második játékos lépése:" << endl;
+    beker();
+    allas();
     return 0;
 }
