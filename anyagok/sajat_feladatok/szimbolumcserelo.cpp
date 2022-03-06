3e0b591d1dc06b495b200f9105222f6c44bb8655
MrHumanRebel <szekelydani5g@gmail.com>
2022-03-06 19:03:17 +0100

    new task BETA

---
+++ anyagok/sajat_feladatok/szimbolumcserelo.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+string tabla[5]{"Gizi", "piros", "kutya", "bicikli", "Karakoszorcsog"};
+string input = 0;
+string uzenet = 0;
+
+void dekod()
+{
+    for (long unsigned int i = 0; i <= input.length(); i++)
+    {
+        uzenet = uzenet + input[i];
+        if (input[i] == '$')
+        {
+            if (input[i + 1] == 'n' && input[i + 2] == 'e' && input[i + 3] == 'v' && input[i + 4] == '$')
+            {
+                uzenet + tabla[1];
+            }
+        }
+    }
+}
+
+void beolv()
+{
+
+    cout << "Adja meg az üzenet szavait \"[vege]\" végjelig!" << endl;
+    cin >> input;
+    dekod();
+}
+
+int main()
+{
+    beolv();
+    cout << uzenet;
+    return 0;
+}
