a260f8451ef2a0c93a40cfe8ec9e7b303756069c
Károly Szakály <karoly.szakaly2000@gmail.com>
2022-03-08 21:07:50 +0100

    feat(sms): add a code function

--- mintavizsgak/2017-18_2/07_sms_dekodolo/sms.cpp
+++ mintavizsgak/2017-18_2/07_sms_dekodolo/sms.cpp
@@ -2,8 +2,10 @@
 #include <fstream>
 using namespace std;
 
+#define BILL_N 12
 #define HIBAS_KAR '>'
-#define TESZT_INPUT "55*3355#29999#334#337777#99996665553#2#333881 1 1"
+#define DEKOD_INPUT "55*3355#29999#334#337777#99996665553#2#333881 1 1"
+#define KOD_INPUT "Kek az eg es zold a fu..."
 
 struct kodPar
 {
@@ -23,32 +25,16 @@ string betolt(string fajlnev)
   return sor;
 }
 
-char ertekKeres(char billentyu, int db)
+char ertekKeres(char billentyu, int db, kodPar kodtabla[BILL_N])
 {
-  kodPar kodtabla[] = {
-      {'1', {'.', ',', '-', '?', '!', '1'}, 6},
-      {'2', {'A', 'B', 'C', '2'}, 4},
-      {'3', {'D', 'E', 'F', '3'}, 4},
-      {'4', {'G', 'H', 'I', '4'}, 4},
-      {'5', {'J', 'K', 'L', '5'}, 4},
-      {'6', {'M', 'N', 'O', '6'}, 4},
-      {'7', {'P', 'Q', 'R', 'S', '7'}, 5},
-      {'8', {'T', 'U', 'V', '8'}, 4},
-      {'9', {'W', 'X', 'Y', 'Z', '9'}, 5},
-      {'*', {'k', 'n', '*'}, 3}, // k == kisbetű, n == nagybetű
-      {'0', {'+', '0'}, 2},
-      {'#', {' ', '#'}, 2},
-  };
-  int kodtablaHossz = sizeof(kodtabla) / sizeof(kodtabla[0]);
-
   int i = 0;
-  while (i < kodtablaHossz && kodtabla[i].billentyu != billentyu)
+  while (i < BILL_N && kodtabla[i].billentyu != billentyu)
   {
     i++;
   }
 
   int keresettIndex = db - 1;
-  if (i < kodtablaHossz && db < kodtabla[i].ertekDb)
+  if (i < BILL_N && db < kodtabla[i].ertekDb)
   {
     return kodtabla[i].ertekek[keresettIndex];
   }
@@ -57,9 +43,9 @@ char ertekKeres(char billentyu, int db)
   return HIBAS_KAR;
 }
 
-string ertekHozzafuz(char kar, int karDb, string szoveg, bool &nagybetus)
+string ertekHozzafuz(char kar, int karDb, kodPar kodtabla[BILL_N], string szoveg, bool &nagybetus)
 {
-  char aktErtek = ertekKeres(kar, karDb);
+  char aktErtek = ertekKeres(kar, karDb, kodtabla);
   switch (aktErtek)
   {
   case 'k':
@@ -82,7 +68,7 @@ string csoportEllenoriz(string szoveg)
   return helyes ? szoveg : "";
 }
 
-string dekodol(string kodoltSzoveg)
+string dekodol(string kodoltSzoveg, kodPar kodtabla[BILL_N])
 {
   string dekodoltSzoveg = "";
   bool nagybetus = true;
@@ -95,7 +81,7 @@ string dekodol(string kodoltSzoveg)
   {
     if (aktKar != kodoltSzoveg[i])
     {
-      dekodoltSzoveg = aktKar == ' ' ? csoportEllenoriz(dekodoltSzoveg) : ertekHozzafuz(aktKar, aktKarDb, dekodoltSzoveg, nagybetus);
+      dekodoltSzoveg = aktKar == ' ' ? csoportEllenoriz(dekodoltSzoveg) : ertekHozzafuz(aktKar, aktKarDb, kodtabla, dekodoltSzoveg, nagybetus);
 
       aktKar = kodoltSzoveg[i];
       aktKarDb = 1;
@@ -111,6 +97,66 @@ string dekodol(string kodoltSzoveg)
   return dekodoltSzoveg;
 }
 
+string billentyuKeres(char ertek, bool &nagybetus, bool ujCsoport, kodPar kodtabla[BILL_N])
+{
+  string eredmeny = ujCsoport ? " " : "";
+
+  /* XOR, csak akkor váltunk, ha eltér egymástól a két érték
+
+     A sima összehasonlítás nem működik,
+     hisz nem 0/1-et vagy true/false értéket ad vissza,
+     hanem egy konkrét int-et (0-t, ha nagybetűs)
+  */
+  if (nagybetus && isupper(ertek) == 0)
+  {
+    eredmeny = isupper(ertek) ? "**" : "*";
+    nagybetus = !nagybetus;
+  }
+
+  char keresettBillentyu;
+  int billentyuDb = 0;
+  bool ertekMegvan = false;
+
+  int i = 0;
+  while (i < BILL_N && !ertekMegvan)
+  {
+    kodPar aktKodPar = kodtabla[i];
+
+    int j = 0;
+    while (j < aktKodPar.ertekDb && !ertekMegvan)
+    {
+      if (aktKodPar.ertekek[j] == toupper(ertek))
+      {
+        keresettBillentyu = kodtabla[i].billentyu;
+        billentyuDb = j + 1;
+        ertekMegvan = true;
+      }
+
+      j++;
+    }
+
+    i++;
+  }
+
+  eredmeny += ertekMegvan ? string(billentyuDb, keresettBillentyu) : string(1, HIBAS_KAR);
+  return eredmeny;
+}
+
+string kodol(string szoveg, kodPar kodtabla[BILL_N])
+{
+  bool nagybetus = isupper(szoveg[0]);
+  string kodoltSzoveg = nagybetus ? "" : "*";
+
+  bool ujCsoport = false;
+  for (size_t i = 0; i < szoveg.length(); i++)
+  {
+    ujCsoport = i > 0 ? szoveg[i - 1] == szoveg[i] : false;
+    kodoltSzoveg += billentyuKeres(szoveg[i], nagybetus, ujCsoport, kodtabla);
+  }
+
+  return kodoltSzoveg;
+}
+
 void mentes(string uzenet, string fajlnev)
 {
   ofstream fajl(fajlnev);
@@ -121,18 +167,33 @@ void mentes(string uzenet, string fajlnev)
 
 int main(int argc, char const *argv[])
 {
+  kodPar kodtabla[BILL_N] = {
+      {'1', {'.', ',', '-', '?', '!', '1'}, 6},
+      {'2', {'A', 'B', 'C', '2'}, 4},
+      {'3', {'D', 'E', 'F', '3'}, 4},
+      {'4', {'G', 'H', 'I', '4'}, 4},
+      {'5', {'J', 'K', 'L', '5'}, 4},
+      {'6', {'M', 'N', 'O', '6'}, 4},
+      {'7', {'P', 'Q', 'R', 'S', '7'}, 5},
+      {'8', {'T', 'U', 'V', '8'}, 4},
+      {'9', {'W', 'X', 'Y', 'Z', '9'}, 5},
+      {'*', {'k', 'n', '*'}, 3}, // k == kisbetű, n == nagybetű
+      {'0', {'+', '0'}, 2},
+      {'#', {' ', '#'}, 2},
+  };
+
   // Első argumentum az inputot tartalmazó szöveges fájl
-  string input = argc == 3 ? betolt(argv[1]) : TESZT_INPUT;
+  string input = argc == 3 ? betolt(argv[1]) : KOD_INPUT;
   cout << "Input: " << input << endl;
 
-  string dekodoltSzoveg = dekodol(input);
+  string eredmeny = kodol(input, kodtabla);
 
   if (argc == 3)
   {
     string outputFajlnev = argv[2];
-    mentes(dekodoltSzoveg, outputFajlnev);
+    mentes(eredmeny, outputFajlnev);
   }
-  cout << "Output: " << dekodoltSzoveg << endl;
+  cout << "Output: " << eredmeny << endl;
 
   return 0;
 }
