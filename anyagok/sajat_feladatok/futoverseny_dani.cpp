151751c2918882ee05a571b92f68045198b41f1e
MrHumanRebel <szekelydani5g@gmail.com>
2022-03-20 20:57:13 +0100

    alpha version

--- anyagok/sajat_feladatok/futoverseny_dani.cpp
+++ anyagok/sajat_feladatok/futoverseny_dani.cpp
@@ -3,28 +3,49 @@
 #include <fstream>
 using namespace std;
 
-#define MAX 15
+#define MAX 20
 
 void olvasas()
 {
-    string futoadat[MAX, MAX];
-    string filename; // Fájl elérési útja
-    int i, db = 0;
-    ifstream futok; // Olvasó rutin nevének megadása
-    /* cout<<"Adja meg a \"futok_nevei.txt\" elérési útját!"<<endl;
-    cin.getline(filename);*/
+    string futoadat[MAX][MAX]; // Futó sorszáma, Futó neve
+    string filename;           // Fájl elérési útja
+
+    string idoadat[MAX][MAX][MAX][MAX][MAX]; // Futó sorszáma, Ellenőrző pont sorszáma, Óra, Perc, Másodperc
+    string filename_2;                       // Fájl elérési útja
+
     filename = "/home/szeke/uni/sze_programozas/anyagok/sajat_feladatok/futok_nevei.txt";
-    futok.open(filename); // Olvasásra megnyitás
-    if (!futok.is_open()) // Ha megnyitás nem sikerült kilépés
+    filename_2 = "/home/szeke/uni/sze_programozas/anyagok/sajat_feladatok/futok_nevei.txt";
+
+    /* cout<<"Adja meg a \"futok_nevei.txt\" elérési útját!"<<endl;
+    cin.getline(filename);
+
+    cout<<"Adja meg a \"idomeres_nevei.txt\" elérési útját!"<<endl;
+    cin.getline(filename_2);*/
+
+    ifstream futok;
+    futok.open(filename);
+
+    if (!futok.is_open())
     {
         cout << "Ilyen fájl nem létezik!" << endl;
         exit(EXIT_FAILURE);
     }
+
     string akt;
     futok >> akt;
-    while (db < 9118 && i <= db && futok.good())
-    {
+    int aktszam;
+    long unsigned int i = 0;
+    int j = 0;
 
+    while (i <= sizeof(futok) && futok.good())
+    {
+        stringstream darab(akt);
+        darab >> aktszam;
+        if (isdigit(aktszam))
+        {
+            futoadat[i][j] += akt;
+            j++;
+        }
         i++;
     }
 }
