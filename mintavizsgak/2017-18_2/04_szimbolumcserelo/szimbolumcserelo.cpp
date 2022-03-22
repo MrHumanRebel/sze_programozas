a370bc9e5453d62465aa2f9790dcda28fc0a6447
Károly Szakály <karoly.szakaly2000@gmail.com>
2022-03-22 20:16:48 +0100

    feat(szimbolum): implement symbol swapping logic

--- mintavizsgak/2017-18_2/04_szimbolumcserelo/szimbolumcserelo.cpp
+++ mintavizsgak/2017-18_2/04_szimbolumcserelo/szimbolumcserelo.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 #define MAX 10
+#define KERESETT_KAR '$'
 
 struct szimbolum
 {
@@ -33,14 +34,11 @@ string szovegBekerese()
   return bemenet;
 }
 
-string szimbolumKeres(string szo, szimbolum szimbolum[MAX])
+string helyorzoCsere(string szo, szimbolum szimbolumok[MAX])
 {
-  char keresettKar = '$';
-  cout << szo << endl;
-
   // Első releváns karakter megkeresése
   size_t i = 0;
-  while (i < szo.length() && szo[i] != keresettKar)
+  while (i < szo.length() && szo[i] != KERESETT_KAR)
   {
     i++;
   }
@@ -49,8 +47,8 @@ string szimbolumKeres(string szo, szimbolum szimbolum[MAX])
     return szo;
 
   // Utolsó releváns karakter megkeresése
-  size_t j = 0;
-  while (j > i && szo[j] != keresettKar)
+  size_t j = szo.length() - 1;
+  while (j > i && szo[j] != KERESETT_KAR)
   {
     j--;
   }
@@ -58,8 +56,26 @@ string szimbolumKeres(string szo, szimbolum szimbolum[MAX])
   if (j <= i)
     return szo;
 
-  string aktHelyorzo = szo.substr(i, j);
-  cout << aktHelyorzo << endl;
+  int helyorzoKezdet = i + 1;
+  int helyorzoVeg = i > 0 ? (j - 1) - i : j - 1;
+
+  string aktHelyorzo = szo.substr(helyorzoKezdet, helyorzoVeg);
+
+  int k = 0;
+  int szimbolumokSzama = sizeof(szimbolumok[0]) / sizeof(szimbolumok);
+  while (k < szimbolumokSzama && szimbolumok[k].helyorzo != aktHelyorzo)
+  {
+    k++;
+  }
+
+  if (k >= szimbolumokSzama)
+    return szo;
+
+  string keresettErtek = szimbolumok[k].ertek;
+  string toldalek = i > 0 ? szo.substr(0, helyorzoKezdet - 1) : szo.substr(j + 1);
+  string keresettCsere = i > 0 ? toldalek + keresettErtek : keresettErtek + toldalek;
+
+  return keresettCsere;
 }
 
 string csere(string bemenet, szimbolum szimbolumok[MAX])
@@ -70,17 +86,18 @@ string csere(string bemenet, szimbolum szimbolumok[MAX])
   bool vanHelyorzo = false;
   for (size_t i = 0; i < bemenet.length(); i++)
   {
+    // TODO: Utolsó rész (":)") hozzáadása
     if (bemenet[i] == ' ')
     {
-      // TODO: Metódus cserére
-      szoveg += vanHelyorzo && aktSzo[1] != '$' ? szimbolumKeres(aktSzo, szimbolumok) + ' ' : aktSzo + ' ';
+      bool csereSzukseges = vanHelyorzo;
+      szoveg += csereSzukseges ? helyorzoCsere(aktSzo, szimbolumok) + ' ' : aktSzo + ' ';
 
       vanHelyorzo = false;
       aktSzo = "";
     }
     else
     {
-      vanHelyorzo = vanHelyorzo || bemenet[i] == '$';
+      vanHelyorzo = vanHelyorzo || bemenet[i] == KERESETT_KAR;
       aktSzo += bemenet[i];
     }
   }
