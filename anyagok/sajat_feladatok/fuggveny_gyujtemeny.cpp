b20e11c40458c7e6521b822e9bc33f1e942f9023
Károly Szakály <karoly.szakaly2000@gmail.com>
2022-03-25 12:49:43 +0100

    fix(fgv-gy): fix board gen and import methods

--- anyagok/sajat_feladatok/fuggveny_gyujtemeny.cpp
+++ anyagok/sajat_feladatok/fuggveny_gyujtemeny.cpp
@@ -6,112 +6,112 @@
 using namespace std;
 #define TELL cout <<
 #define ASK cin >>
+#define SOR_MAX 10
+#define OSZLOP_MAX 10
 
 // Szám bekerő függvény
-int szambeker(int MIN, int MAX)
+int szamBeker(int MIN, int MAX)
 {
-    bool szambe_ok = false;
-    int akt;
-    do
-    {
-        TELL "Adja meg a számot!" << endl;
-        ASK akt;
-        if (akt >= MIN && akt <= MAX)
-            szambe_ok = true;
-        else
-            TELL "Adjon meg " << MIN << "-től " << MAX << "-ig tartományba tartozó számot!" << endl;
-    } while (szambe_ok == false);
-    return akt;
+  bool szambe_ok = false;
+  int akt;
+  do
+  {
+    TELL "Adja meg a számot!" << endl;
+    ASK akt;
+    if (akt >= MIN && akt <= MAX)
+      szambe_ok = true;
+    else
+      TELL "Adjon meg " << MIN << "-től " << MAX << "-ig tartományba tartozó számot!" << endl;
+  } while (szambe_ok == false);
+  return akt;
 }
 
 // Karakter bekerő függvény
-int karbeker(char KEZD, char VEG)
+int karBeker(char KEZD, char VEG)
 {
-    bool karbe_ok = false;
-    char akt;
-    do
-    {
-        TELL "Adja meg a karaktert!" << endl;
-        ASK akt;
-        // akt = tolower(akt);
-        // akt = toupper(akt);
-        if (akt >= KEZD && akt <= VEG)
-            karbe_ok = true;
-        else
-            TELL "Adjon meg " << KEZD << "-től " << VEG << "-ig tartományba tartozó karaktert!" << endl;
-    } while (karbe_ok == false);
-    return akt;
+  bool karbe_ok = false;
+  char akt;
+  do
+  {
+    TELL "Adja meg a karaktert!" << endl;
+    ASK akt;
+    // akt = tolower(akt);
+    // akt = toupper(akt);
+    if (akt >= KEZD && akt <= VEG)
+      karbe_ok = true;
+    else
+      TELL "Adjon meg " << KEZD << "-től " << VEG << "-ig tartományba tartozó karaktert!" << endl;
+  } while (karbe_ok == false);
+  return akt;
 }
 
 // Pálya generáló függvény
 // BETA VERSION
-char palya_gen(int SORMAX, int OSZLMAX, char tabla[][], bool van_palya)
+void palyaGen(char tabla[SOR_MAX][OSZLOP_MAX], int sor, int oszlop)
 {
 
-    for (int i = 0; i < SORMAX; i++)
+  for (int i = 0; i < sor; i++)
+  {
+    for (int j = 0; j < oszlop; j++)
     {
-        for (int j = 0; j < OSZLMAX; j++)
-        {
-            tabla[i][j] = '.';
-        }
+      tabla[i][j] = '.';
     }
-    van_palya = true;
-    return tabla[][];
+  }
+}
+
+void palyaBeolvas(char palya[SOR_MAX][OSZLOP_MAX], int &sor, int &oszlop, string fajlnev)
+{
+  // TODO: Implementáld
+  ifstream mentettPalya(fajlnev);
+
+  string aktSor;
+
+  // A fájl első két sora a pálya sorainak és az oszlopainak száma
+  getline(mentettPalya, aktSor);
+  sor = stoi(aktSor);
+
+  getline(mentettPalya, aktSor);
+  stoi(aktSor);
+
+  for (int i = 0; i < sor; i++)
+  {
+    getline(mentettPalya, aktSor);
+
+    for (int j = 0; j < oszlop; j++)
+    {
+      palya[i][j] = aktSor[j];
+    }
+  }
+
+  mentettPalya.close();
 }
 
 // Alap karakter olvasó függvény
 // BETA VERSION
-char olvasas(int SORMAX, int OSZLMAX, string olvashely, char tabla[][], bool van_palya)
-{
-    string olvashely;
-    ifstream olvas;
-    int tovabb = 0;
-    char akt;
-    olvas.open(olvashely);
-    if (!olvas.is_open())
-    {
-        TELL "Nincs mentett állás, vagy a fájl nem elérhető!\n"
-            << endl;
-        van_palya = false;
-    }
-    else
-    {
-        van_palya = true;
-        for (int i = 0; i <= SORMAX; i++)
-        {
-            for (int j = 0; j <= OSZLMAX; j++)
-            {
-                olvas >> akt;
-                tabla[i][j] = akt;
-            }
-        }
-    }
-    olvas.close();
-}
 
 int main()
 {
-    // Szám bekerő függvény tesztelő
-    int MAX = 10;
-    int MIN = 1;
-    int szam_akt = szambeker(MIN, MAX);
-    TELL szam_akt << endl;
+  // Szám bekerő függvény tesztelő
+  int MAX = 10;
+  int MIN = 1;
+  int szam_akt = szamBeker(MIN, MAX);
+  TELL szam_akt << endl;
 
-    // Karakter bekerő függvény tesztelő
-    char KEZD = 'A';
-    char VEG = 'D';
-    char kar_akt = karbeker(KEZD, VEG);
-    TELL kar_akt << endl;
+  // Karakter bekerő függvény tesztelő
+  char KEZD = 'A';
+  char VEG = 'D';
+  char kar_akt = karBeker(KEZD, VEG);
+  TELL kar_akt << endl;
 
-    // Pálya generáló függvény tesztelő
-    int SORMAX = 10;
-    int OSZLMAX = 10;
-    bool van_palya = false;
-    char tabla[SORMAX][OSZLMAX] = palya_gen(SORMAX, OSZLMAX, tabla[][], van_palya);
+  // Pálya generáló függvény tesztelő
+  int sor = 10;
+  int oszlop = 10;
+  char tabla[SOR_MAX][OSZLOP_MAX];
+  palyaGen(tabla, sor, oszlop);
 
-    // Alap karakter olvasó függvény tesztelő
-    string olvashely = "/home/szeke/uni/sze_programozas/anyagok/sajat_feladatok/amoba.txt";
-    char tabla[SORMAX][OSZLMAX] = olvasas(SORMAX, OSZLMAX, olvashely, tabla[][], van_palya);
+  // Alap karakter olvasó függvény tesztelő
+  string fajlnev = "/home/szeke/uni/sze_programozas/anyagok/sajat_feladatok/amoba.txt";
+  palyaBeolvas(tabla, sor, oszlop, fajlnev);
 
-    return 0;
+  return 0;
 }
