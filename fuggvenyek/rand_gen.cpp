1aa1b584321e45c5eb8fff7bc46d70891e75941c
MrHumanRebel <szekelydani5g@gmail.com>
2022-04-12 09:02:52 +0200

    add rand gen

---
+++ fuggvenyek/rand_gen.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+#define TELL cout <<
+#define ASK cin >>
+#define MIN 5
+#define MAX 50
+
+// Random szám generátor
+int random(int tol, int ig)
+{
+    return (tol + rand() % (ig - tol + 1));
+}
+
+int main()
+{
+    // Random szám generátor tesztelő
+    int szam = random(MIN, MAX);
+    TELL szam;
+    return 0;
+}
