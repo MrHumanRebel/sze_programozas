a6205bd079b9a3a46ee94b5d9069a08a2d39328d
Károly Szakály <karoly.szakaly2000@gmail.com>
2022-04-12 19:44:54 +0200

    feat(lib): add recursive factorial calc

---
+++ fuggvenyek/faktorialis.cpp
@@ -0,0 +1,19 @@
+/* Rekurzív faktoriális függvény */
+#include <iostream>
+using namespace std;
+
+int faktorialis(int n)
+{
+  if (n == 0)
+    return 1;
+
+  return n * faktorialis(n - 1);
+}
+
+int main(int argc, char const *argv[])
+{
+  int fakt = faktorialis(5);
+  cout << fakt << endl;
+
+  return 0;
+}
