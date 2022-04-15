af81ff9584ae7ac93445167bcc91871180de6959
MrHumanRebel <szekelydani5g@gmail.com>
2022-04-15 14:32:12 +0200

    add beta functions

--- mintavizsgak/2019-20_1/01_memoriajatek/memjatek_dani.cpp
+++ mintavizsgak/2019-20_1/01_memoriajatek/memjatek_dani.cpp
@@ -34,97 +34,70 @@ int szamBeker(int min, int max)
 
 void feltolt_eleje(char tomb[SOR_MAX][OSZL_MAX])
 {
-    int a = 0, b = 0, c = 0, x = 0;
+    int a = 0, b = 0, c = 0;
     for (int i = 0; i < SOR_MAX; i++)
     {
         for (int j = 0; j < OSZL_MAX; j++)
         {
-            seed = random(1, 1000);
+            seed = random(1, 50);
             tomb[i][j] = random('a', 'c');
 
             if (tomb[i][j] == 'a')
+            {
                 a++;
+                if (a >= 3)
+                {
+                    do
+                    {
+                        seed = random(1000, 2000);
+                        tomb[i][j] = random('b', 'c');
+                    } while (tomb[i][j] != 'a');
+
+                    a--;
+                    if (tomb[i][j] == 'b')
+                        b++;
+                    else if (tomb[i][j] == 'c')
+                        c++;
+                }
+            }
             else if (tomb[i][j] == 'b')
+            {
                 b++;
+                if (b >= 3)
+                {
+                    do
+                    {
+                        seed = random(3000, 4000);
+                        tomb[i][j] = random('a', 'c');
+                    } while (tomb[i][j] != 'b');
+
+                    b--;
+                    if (tomb[i][j] == 'a')
+                        a++;
+                    else if (tomb[i][j] == 'c')
+                        c++;
+                }
+            }
             else if (tomb[i][j] == 'c')
+            {
                 c++;
+                if (c >= 3)
+                {
+                    do
+                    {
+                        seed = random(4000, 5000);
+                        tomb[i][j] = random('a', 'b');
+                    } while (tomb[i][j] != 'c');
+
+                    c--;
+                    if (tomb[i][j] == 'a')
+                        a++;
+                    else if (tomb[i][j] == 'b')
+                        b++;
+                }
+            }
 
             // TELL "a: " << a_db << " b: " << b_db << " c: " << c_db << endl;
-
-            if (a >= 3)
-            {
-                do
-                {
-                    seed = random(1000, 2000);
-                    tomb[i][j] = random('b', 'c');
-                    x++;
-                    if (x > 100)
-                    {
-                        continue;
-                    }
-                } while (tomb[i][j] != 'a');
-
-                if (x > 100)
-                {
-                    continue;
-                }
-                a--;
-                if (tomb[i][j] == 'b')
-                    b++;
-                else if (tomb[i][j] == 'c')
-                    c++;
-            }
-
-            x = 0;
-
-            if (b >= 3)
-            {
-                do
-                {
-                    seed = random(3000, 4000);
-                    tomb[i][j] = random('a', 'c');
-                    if (x > 100)
-                    {
-                        continue;
-                    }
-                } while (tomb[i][j] != 'b');
-
-                if (x > 100)
-                {
-                    continue;
-                }
-                b--;
-                if (tomb[i][j] == 'a')
-                    a++;
-                else if (tomb[i][j] == 'c')
-                    c++;
-            }
-
-            x = 0;
-
-            if (c >= 3)
-            {
-                do
-                {
-                    seed = random(4000, 5000);
-                    tomb[i][j] = random('a', 'b');
-                    if (x > 100)
-                    {
-                        continue;
-                    }
-                } while (tomb[i][j] != 'c');
-
-                if (x > 100)
-                {
-                    continue;
-                }
-                c--;
-                if (tomb[i][j] == 'a')
-                    a++;
-                else if (tomb[i][j] == 'b')
-                    b++;
-            }
-            x = 0;
         }
     }
 }
@@ -142,7 +115,7 @@ void feltolt_hatulja(char tomb[SOR_MAX][OSZL_MAX])
 
 void kiir(char tomb[SOR_MAX][OSZL_MAX])
 {
-    TELL "  123"
+    TELL "♤ 123"
         << "\n─────" << endl;
     int x = 1;
     for (int i = 0; i < SOR_MAX; i++)
@@ -157,29 +130,52 @@ void kiir(char tomb[SOR_MAX][OSZL_MAX])
     }
 }
 
-void beker()
+void beker(int sor, int oszlop)
 {
     TELL "\nMegforditando kartya sora: ";
-    szamBeker(1, 2);
+    sor = szamBeker(1, 2);
     TELL "Oszlopa: ";
-    szamBeker(1, 3);
+    oszlop = szamBeker(1, 3);
 }
 
-void fordit(int ok)
+char ellenoriz(char tomb[SOR_MAX][OSZL_MAX], int sor, int oszlop)
 {
+    char akt = tomb[sor - 1][oszlop - 1];
+    return akt;
 }
 
 int main()
 {
     TELL "Memoriajatek\n"
         << endl;
+    // Inicializáció
+    int ok = 0, sor = '\0', oszlop = '\0', elozosor = '\0', elozooszlop = '\0';
     char kartyak_eleje[SOR_MAX][OSZL_MAX];
     feltolt_eleje(kartyak_eleje);
     char kartyak_hatulja[SOR_MAX][OSZL_MAX];
     feltolt_hatulja(kartyak_hatulja);
-    int ok = 0;
-    kiir(kartyak_eleje);
-    beker();
+
+    kiir(kartyak_hatulja);
+    do
+    {
+        beker(sor, oszlop);
+        char elozo = ellenoriz(kartyak_eleje, sor, oszlop);
+        elozosor = sor;
+        elozooszlop = oszlop;
+        beker(sor, oszlop);
+        char akt = ellenoriz(kartyak_eleje, sor, oszlop);
+
+        if (akt == elozo)
+        {
+            ok++;
+            TELL "Egyformak, a kartyak igy maradnak." << endl;
+            kartyak_hatulja[elozosor][elozooszlop] = elozo;
+            kartyak_hatulja[sor][oszlop] = akt;
+            kiir(kartyak_hatulja);
+        }
+        else
+            TELL "Nem egyeznek, visszaforditom a lapokat." << endl;
+    } while (ok != 3);
 
     return 0;
 }
