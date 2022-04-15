435645811556f453009d106b62d9b6ed53ad635f
MrHumanRebel <szekelydani5g@gmail.com>
2022-04-15 09:22:17 +0200

    add not working ATM tomb meret

--- fuggvenyek/tomb_meret.cpp
+++ fuggvenyek/tomb_meret.cpp
@@ -1,28 +1,16 @@
 #include <iostream>
 using namespace std;
 #define MAX 10
-int seed = 0;
 
-int tomb_meret(char tomb[])
+int tomb_meret(int tomb[])
 {
-    int n = sizeof(tomb) / sizeof(tomb[0]);
+    int n = *(&tomb + 1) - tomb; // Not working ATM
     return n;
-}
-
-int random(int tol, int ig)
-{
-    srand(time(NULL) + seed);
-    return (tol + rand() % (ig - tol + 1));
 }
 
 int main()
 {
-    char tabla[MAX];
-    for (int i = 0; i <= MAX; i++)
-    {
-        seed = random(1, 50);
-        tabla[i] = random('A', 'Z');
-    }
-    cout << tomb_meret(tabla);
+    int tomb[MAX]{10, 20, 30, 40, 50, 60};
+    cout << tomb_meret(tomb);
     return 0;
 }
