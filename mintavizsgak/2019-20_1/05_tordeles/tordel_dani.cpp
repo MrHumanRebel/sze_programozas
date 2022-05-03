72e5f0d8152545671b20be45780a7f80af9c98d8
MrHumanRebel <szekelydani5g@gmail.com>
2022-05-03 19:54:20 +0200

    fixes

--- mintavizsgak/2019-20_1/05_tordeles/tordel_dani.cpp
+++ mintavizsgak/2019-20_1/05_tordeles/tordel_dani.cpp
@@ -8,7 +8,7 @@ using namespace std;
 #define ASK cin >>
 #define MAX 20
 #define VEGEJEL "[vege]"
-// 12345678901234567890 Ez itt egy nagyon hosszu szoveg, amit balra igazitva be kell tordelni a rendelkezesre allo, 20 karakter szeles helyre. [vege]
+// Ez itt egy nagyon hosszu szoveg, amit balra igazitva be kell tordelni a rendelkezesre allo, 20 karakter szeles helyre. [vege]
 
 string beolv()
 {
@@ -18,26 +18,64 @@ string beolv()
     return input;
 }
 
+void oszlopir()
+{
+    int ciklus = 0;
+    TELL '*';
+    while (ciklus != 2)
+    {
+        for (int i = 0; i <= MAX / 2 - 1; i++)
+        {
+            TELL i;
+        }
+        ciklus++;
+    }
+    TELL '*' << endl;
+}
+
 void kiir(string szoveg)
 {
     int db = szoveg.length();
-    int aktdb = db;
-    TELL "Szöveg hossza:" << aktdb << endl;
-    int aktkar = 0;
-    while (aktdb > 0)
+    string akt = "\0";
+    int szamlalo = 0;
+    string sorok[MAX];
+    for (int i = 0; i < MAX; i++)
     {
-        TELL '*';
-        for (int i = 0; i < MAX; i++)
+        sorok[i] = "\0";
+    }
+    int j = 0;
+    for (int i = 0; i < db; i++)
+    {
+        if (szoveg[i] != ' ')
         {
-            if (aktkar >= db)
-                TELL ' ';
-            else
-                TELL szoveg[aktkar];
-            aktkar++;
-            aktdb--;
+            akt += szoveg[i];
+            szamlalo++;
+            if (szamlalo >= MAX)
+            {
+                szamlalo = 0;
+                j++;
+            }
         }
+        else
+        {
+            sorok[j] = sorok[j] + akt + ' ';
+            szamlalo++;
+            akt = "\0";
+        }
+    }
+
+    oszlopir();
+    for (int i = 0; i <= j; i++)
+    {
+        TELL '*' << sorok[i];
+        if (sorok[i].length() < MAX)
+            for (size_t k = 0; k < MAX - sorok[i].length(); k++)
+            {
+                TELL ' ';
+            }
         TELL '*' << endl;
     }
+    oszlopir();
 }
 
 int main()
