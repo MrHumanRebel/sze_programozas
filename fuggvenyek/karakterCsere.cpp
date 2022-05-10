311a8c7653e9e49e1817636440e5d84a09179f9a
Rivalser <97056846+Rivalser@users.noreply.github.com>
2022-05-10 10:08:01 +0200

    Update and rename karakterekÖsszElőfordulásánakCseréi.cpp to karakterCsere.cpp

--- fuggvenyek/karakterCsere.cpp
+++ fuggvenyek/karakterCsere.cpp
@@ -4,32 +4,23 @@
 using namespace std;
 // C++: You just can not change/delete the space chars from a string to manage all the signs in it. This is illegal..
 
-string charsChangeInString(string szoveg, char mit, char mire) {
+string replaceChar(string szoveg, char mit, char mire) {
 
 	int n = szoveg.length();
 	string modositottSzoveg = "";
 
-	char* tomb = new char[n + 1];
-
-	for (int i = 0; i < n; i++) {
-		tomb[i] = szoveg[i];
-	}
-
 	for (int i = 0; i < n; i++)
 	{
-		if (tomb[i] == mit) {
-			tomb[i] = mire;
-		}
-		modositottSzoveg += tomb[i];
+		modositottSzoveg += szoveg[i] == mit ? mire : szoveg[i];
 	}
+	
 	return modositottSzoveg;
 }
 
 int main()
 {
 	string s = "Haha gen";
-	string vmi = "";
-	vmi = charsChangeInString(s, ' ', '!');
+	string vmi = replaceChar(s, ' ', '!');
 	cout << vmi;
 
 
