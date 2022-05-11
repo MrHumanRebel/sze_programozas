963407c2d44aeb9e1a4021609b093f54c8a35b21
Károly Szakály <karoly.szakaly2000@gmail.com>
2022-05-11 18:11:00 +0200

    feat(receptek): initial commit

---
+++ mintavizsgak/2018-19_2/01_receptek/receptek_karcsi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+#define KEZDO_KISERO "Fozesi lehetosegeket analizalo program."
+
+#define ALAPANYAG_KISERO "Milyen alapanyagok allnak rendelkezesre?"
+#define ETEL_KISERO "Milyen alapanyagokra van szukseg?"
+#define INPUT_KISERO "Befejezes #-tel."
+
+#define INPUT_VEGE '#'
+
+
+int main(int argc, char const *argv[])
+{
+  cout << KEZDO_KISERO << endl;
+
+  return 0;
+}
