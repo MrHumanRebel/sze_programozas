1ba30870dc7571509eaa7cecf19de23a5a65ba8a
Károly Szakály <karoly.szakaly2000@gmail.com>
2022-05-11 19:24:32 +0200

    feat(receptek): impl basic linked list operations

--- mintavizsgak/2018-19_2/01_receptek/receptek_karcsi.cpp
+++ mintavizsgak/2018-19_2/01_receptek/receptek_karcsi.cpp
@@ -10,10 +10,66 @@ using namespace std;
 
 #define INPUT_VEGE '#'
 
+struct alapanyag
+{
+  string nev;
+  alapanyag *elozo;
+  alapanyag *kov;
+};
+
+alapanyag *beszur(string nev, alapanyag *horgony)
+{
+  if (!horgony)
+  {
+    alapanyag *uj = new alapanyag;
+    uj->nev = nev;
+    uj->elozo = uj->kov = NULL;
+
+    return uj;
+  }
+
+  if (nev.compare(horgony->nev) > 0)
+  {
+    horgony->kov = beszur(nev, horgony->kov);
+  }
+  else
+  {
+    horgony->elozo = beszur(nev, horgony->elozo);
+  }
+
+  return horgony;
+}
+
+void kiirRendezett(alapanyag *horgony)
+{
+  if (!horgony)
+  {
+    return;
+  }
+
+  kiirRendezett(horgony->elozo);
+  cout << horgony->nev << endl;
+  kiirRendezett(horgony->kov);
+}
+
+void torolMind(alapanyag *horgony)
+{
+
+  while (horgony)
+  {
+    alapanyag *kovSzimb = horgony->kov;
+    delete horgony;
+    horgony = kovSzimb;
+  }
+}
 
 int main(int argc, char const *argv[])
 {
   cout << KEZDO_KISERO << endl;
+
+  alapanyag *horgony = NULL;
+
+  torolMind(horgony);
 
   return 0;
 }
