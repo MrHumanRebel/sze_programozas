34f21cf0f8eb757b9deba2ec78ac276dd755ebff
Károly Szakály <karoly.szakaly2000@gmail.com>
2022-05-13 10:32:17 +0200

    feat(plagium): initial commit

---
+++ mintavizsgak/2018-19_2/03_plagiumellenorzo/plagium_karcsi.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+#define KEZDO "Egyszeru plagiumellenorzo szoftver; kilepes ures sor megadasaval."
+
+int main(int argc, char const *argv[])
+{
+  cout << KEZDO << endl;
+  return 0;
+}
