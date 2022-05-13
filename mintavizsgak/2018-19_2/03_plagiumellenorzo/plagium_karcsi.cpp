a27d273d9c3ae9f8045ff4a5356cb6f0b24a2792
Károly Szakály <karoly.szakaly2000@gmail.com>
2022-05-13 10:38:12 +0200

    feat(plagium): impl a singly linked list of publications

--- mintavizsgak/2018-19_2/03_plagiumellenorzo/plagium_karcsi.cpp
+++ mintavizsgak/2018-19_2/03_plagiumellenorzo/plagium_karcsi.cpp
@@ -4,6 +4,39 @@ using namespace std;
 
 #define KEZDO "Egyszeru plagiumellenorzo szoftver; kilepes ures sor megadasaval."
 
+struct mu
+{
+  string tudosNeve;
+  string datum;
+  string tartalom;
+
+  mu *kov;
+};
+
+mu *beszur(string tudosNeve, string datum, string tartalom, mu *elozo)
+{
+  mu *uj = new mu;
+
+  if (uj)
+  {
+    uj->tudosNeve = tudosNeve;
+    uj->datum = datum;
+    uj->tartalom = tartalom;
+
+    if (elozo)
+    {
+      uj->kov = elozo->kov;
+      elozo->kov = uj;
+    }
+    else
+    {
+      uj->kov = NULL;
+    }
+  }
+
+  return uj;
+}
+
 int main(int argc, char const *argv[])
 {
   cout << KEZDO << endl;
