95c92f3235261f00603125df29c47eb016cd083f
Károly Szakály <karoly.szakaly2000@gmail.com>
2022-05-18 21:16:56 +0200

    fix(lib): add missing extension, move to correct folder

--- fuggvenyek/szoveg/betubol_szam.cpp
+++ fuggvenyek/szoveg/betubol_szam.cpp
@@ -1,14 +1,3 @@
-string szokozkiszed(const string &s){
-    int hosssz = s.length();
-    string vissza;
-    for(int i = 0; i < hosssz; i++){
-        if (s[i] != ' ') {
-            vissza += s[i];
-        }
-    }
-    return vissza;
-}
-
 void betubolszam(const string &s){
     int hosssz = s.length();
     for(int i = 0; i < hosssz; i++){
