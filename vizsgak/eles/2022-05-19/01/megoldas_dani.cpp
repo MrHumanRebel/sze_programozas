88a8ec6dbd5fba09a97ffb3c291dab0163b26332
Károly Szakály <karoly.szakaly2000@gmail.com>
2022-05-19 21:12:53 +0200

    chore(2022-05-19): format README

--- vizsgak/eles/2022-05-19/01/megoldas_dani.cpp
+++ vizsgak/eles/2022-05-19/01/megoldas_dani.cpp
@@ -5,70 +5,70 @@ using namespace std;
 
 int beker(int be_tb[], int *min, int *max)
 {
-    int szum = 0;
-    int aktszam;
-    int i = 0;
-    bool ok = false;
-    do
+  int szum = 0;
+  int aktszam;
+  int i = 0;
+  bool ok = false;
+  do
+  {
+    ok = false;
+    if (i == 0) // Nulla
     {
-        ok = false;
-        if (i == 0) // Nulla
-        {
-            do
-            {
-                cin >> aktszam;
-                if (aktszam == 0)
-                    ok = true;
+      do
+      {
+        cin >> aktszam;
+        if (aktszam == 0)
+          ok = true;
 
-            } while (ok == false);
-            be_tb[i] = aktszam;
-        }
-        else if (i % 2 == 0) // Páros
-        {
-            do
-            {
-                cin >> aktszam;
-                if (aktszam % 2 == 0)
-                    ok = true;
+      } while (ok == false);
+      be_tb[i] = aktszam;
+    }
+    else if (i % 2 == 0) // Páros
+    {
+      do
+      {
+        cin >> aktszam;
+        if (aktszam % 2 == 0)
+          ok = true;
 
-            } while (ok == false);
-            be_tb[i] = aktszam;
-            szum += aktszam;
-            if (aktszam > *max)
-                *max = aktszam;
-        }
-        else if (i % 2 != 0) // Páratlan
-        {
-            do
-            {
-                cin >> aktszam;
-                if (aktszam % 2 != 0)
-                    ok = true;
+      } while (ok == false);
+      be_tb[i] = aktszam;
+      szum += aktszam;
+      if (aktszam > *max)
+        *max = aktszam;
+    }
+    else if (i % 2 != 0) // Páratlan
+    {
+      do
+      {
+        cin >> aktszam;
+        if (aktszam % 2 != 0)
+          ok = true;
 
-            } while (ok == false);
-            be_tb[i] = aktszam;
-            szum += aktszam;
-            if (aktszam < *min)
-                *min = aktszam;
-        }
-        i++;
-    } while (i < 10);
-    return szum;
+      } while (ok == false);
+      be_tb[i] = aktszam;
+      szum += aktszam;
+      if (aktszam < *min)
+        *min = aktszam;
+    }
+    i++;
+  } while (i < 10);
+  return szum;
 }
 
 void kiir(int szum, int min, int max)
 {
-    cout << "Szum:" << szum << "\nPrtln min:" << min << "\nPrs max:" << max;
+  cout << "Szum:" << szum << "\nPrtln min:" << min << "\nPrs max:" << max;
 }
 
 int main()
 {
-    int min = INT_MAX;
-    int max INT_MIN;
+  int min = INT_MAX;
+  int max INT_MIN;
 
-    int tomb[TOMBMAX];
-    int szum = beker(tomb, &min, &max);
-    kiir(szum, min, max);
+  int tomb[TOMBMAX];
+  int szum = beker(tomb, &min, &max);
+  kiir(szum, min, max);
 
-    return 0;
+  return 0;
 }
