590d9a2978bc04bed948d17ba6bb5093c8d4af2d
Debreczeni Kálmán <debreczeni.kalman14@gmail.com>
2022-06-25 21:59:53 +0200

    Expanded DFK
    Refactor: DLinkedList push function split into 2: PushAfter and PushBefore
    New: DDelegate event handler

--- fuggvenyek/DFK.cpp
+++ fuggvenyek/DFK.cpp
@@ -88,7 +88,7 @@ public:
 
 	~LinkedListNode()
 	{
-
+		delete data;
 	}
 
 private:
@@ -147,14 +147,16 @@ public:
 	}
 
 	// Need help in this one. Doesn't recognize [] as an operator
-	//LinkedListNode<T>* operator[](int index)
-	//{
-	//	int revert = index;
-	//	SeekToIndex(index);
-	//	LinkedListNode<T>* tmp = currentElement;
-	//	SeekToIndex(revert);
-	//	return tmp;
-	//}
+
+
+	// LinkedListNode<T>* operator[](int index)     // nem ismeri fel []-t mint oerátort
+	// {
+	// 	int revert = index;
+	// 	SeekToIndex(index);
+	// 	LinkedListNode<T>* tmp = currentElement;
+	// 	SeekToIndex(revert);
+	// 	return tmp;
+	// }
 
 	/// <summary>
 	/// Seeks to the specified index in the list, making the nth element active
@@ -162,6 +164,10 @@ public:
 	/// <param name="newIndex">The index to seek to</param>
 	void SeekToIndex(int newIndex)
 	{
+		if (newIndex < 0 || newIndex >= count)
+		{
+			return;
+		}
 		if (currentIndex == newIndex)
 		{
 			return;
@@ -248,6 +254,7 @@ public:
 		return true;
 	}
 
+#if false
 	/// <summary>
 	/// Pushes data to the list, before the active element
 	/// </summary>
@@ -309,6 +316,91 @@ public:
 		}
 	}
 
+#endif // obsolete content
+
+	/// <summary>
+	/// Pushes data before the current element and remains positioned on the old one
+	/// </summary>
+	/// <param name="data">Data to be stored</param>
+	/// <returns>Whether the push was sucessful</returns>
+	bool PushBefore(T data)
+	{
+		if (count == 0)
+		{
+			LinkedListNode<T>* n = new LinkedListNode<T>(data);
+			if (n != nullptr)
+			{
+				firstElement = n;
+				currentElement = n;
+				lastElement = n;
+				currentIndex = 0;
+				count = 1;
+				return true;
+			}
+			else return false;
+		}
+
+		if (currentIndex == 0)
+		{
+			currentIndex -= 1;
+			return PushFirst(data);
+		}
+		else
+		{
+			LinkedListNode<T>* n = new LinkedListNode<T>(data);
+			if (n != nullptr)
+			{
+				n->nextElement = currentElement;
+				n->previousElement = currentElement->previousElement;
+
+				currentElement->previousElement->nextElement = n;
+				currentElement->previousElement = n;
+
+				count++;
+
+				return true;
+			}
+
+
+			else return false;
+		}
+	}
+
+	/// <summary>
+	/// Pushes data after the current element and positions on the new element
+	/// </summary>
+	/// <param name="data">Data to be stored</param>
+	/// <returns>Whether the push was sucessful</returns>
+	bool PushAfter(T data)
+	{
+		if (currentIndex == count - 1 || count == 0)
+		{
+			currentIndex++;
+			return PushLast(data);
+		}
+		else
+		{
+			LinkedListNode<T>* n = new LinkedListNode<T>(data);
+			if (n != nullptr)
+			{
+				n->previousElement = currentElement;
+				n->nextElement = currentElement->nextElement;
+
+				currentElement->nextElement->previousElement = n;
+				currentElement->nextElement = n;
+
+				currentIndex++;
+				count++;
+
+				return true;
+			}
+
+
+			else return false;
+		}
+	}
+
+
 	/// <summary>
 	/// Pushes data to the head of the list
 	/// </summary>
@@ -318,20 +410,30 @@ public:
 	{
 		if (count == 0)
 		{
-			return Push(data);
+			LinkedListNode<T>* n = new LinkedListNode<T>(data);
+			if (n != nullptr)
+			{
+				firstElement = n;
+				currentElement = n;
+				lastElement = n;
+				currentIndex = 0;
+				count = 1;
+				return true;
+			}
+			else return false;
 		}
 		else
 		{
 			LinkedListNode<T>* n = new LinkedListNode<T>(data);
 			if (n != nullptr)
 			{
-				firstElement->previousElement = n;
 				n->nextElement = firstElement;
+				firstElement->previousElement = n;
 
 				firstElement = n;
 
-				currentIndex++;
 				count++;
+				currentIndex++;
 			}
 			else return false;
 		}
@@ -346,7 +448,17 @@ public:
 	{
 		if (count == 0)
 		{
-			return Push(data);
+			LinkedListNode<T>* n = new LinkedListNode<T>(data);
+			if (n != nullptr)
+			{
+				firstElement = n;
+				currentElement = n;
+				lastElement = n;
+				currentIndex = 0;
+				count = 1;
+				return true;
+			}
+			else return false;
 		}
 		else
 		{
@@ -374,7 +486,7 @@ public:
 	{
 		int revert = currentIndex;
 		SeekToIndex(index);
-		bool tmp = Push(data);
+		bool tmp = PushAfter(data);
 		SeekToIndex(revert);
 		return tmp;
 
@@ -387,6 +499,10 @@ public:
 	void RemoveAt(int index)
 	{
 		int revert = currentIndex;
+		if (index < revert)
+		{
+			revert--;
+		}
 		SeekToIndex(index);
 		if (index == 0) // first element
 		{
@@ -479,22 +595,243 @@ private:
 // TODO: Intensive and extensive testing
 
 
+/// <summary>
+/// Delegate action, containing the pointer to a function and other information
+/// </summary>
+/// <typeparam name="Return">Type of the function (void, int, char...)</typeparam>
+/// <typeparam name="...Params">Flexible number of function parameter types</typeparam>
+template<class Return, class... Params>
+class DDelegateAction
+{
+public:
+	DDelegateAction()
+	{
+		ID = 0;
+		timesCalled = 0;
+	}
+	DDelegateAction(int ID)
+	{
+		this->ID = ID;
+		timesCalled = 0;
+	}
+	DDelegateAction(int ID, Return(*function)(Params...))
+	{
+		this->functionPointer = function;
+		this->ID = ID;
+		timesCalled = 0;
+	}
+	DDelegateAction(Return(*function)(Params...))
+	{
+		this->functionPointer = function;
+		this->ID = 0;
+		timesCalled = 0;
+	}
+	~DDelegateAction()
+	{
+
+	}
+
+	Return Fire(Params... p)
+	{
+		timesCalled++;
+		return functionPointer(p...);
+	}
+
+	Return(*functionPointer)(Params...);
+	int ID;
+	int timesCalled;
+	/*Anything more?*/
+
+private:
+};
+
+
+/// <summary>
+/// Delegate event handler
+/// </summary>
+/// <typeparam name="Return">Type of the function (void, int, char...)</typeparam>
+/// <typeparam name="...Params">Flexible number of function parameter types</typeparam>
+template <class Return, class... Params>
+class DDelegate
+{
+public:
+
+	DDelegate()
+	{
+		_CrtDumpMemoryLeaks();
+		actionList = new DLinkedList<DDelegateAction<Return, Params...>*>();
+		_CrtDumpMemoryLeaks();
+	}
+	~DDelegate()
+	{
+		_CrtDumpMemoryLeaks();
+		delete actionList;
+		_CrtDumpMemoryLeaks();
+
+	}
+
+	/// <summary>
+	/// Fires every function in subscribe order but deletes their returned value
+	/// </summary>
+	/// <param name="...p">Parameters for the functions to use</param>
+	void FireAllWithoutReturns(Params... p)
+	{
+		int length = actionList->Count();
+		Return* retArray = new Return[length];
+		for (int i = 0; i < length; i++)
+		{
+			retArray[i] = actionList->GetDataAt(i)->Fire(p...);
+		}
+		delete retArray;
+	}
+
+	/// <summary>
+	/// Fires every function in subscribe order
+	/// </summary>
+	/// <param name="...p">Parameters for the functions to use</param>
+	/// <returns>An array of the returned value of the functions</returns>
+	Return* FireAll(Params... p)
+	{
+		if (typeid(Return) == typeid(void))
+		{
+			int length = actionList->Count();
+			for (int i = 0; i < length; i++)
+			{
+				actionList->GetDataAt(i)->Fire(p...);
+			}
+			return NULL;
+		}
+		else
+		{
+			int length = actionList->Count();
+			Return* retArray = new Return[length];
+			for (int i = 0; i < length; i++)
+			{
+				retArray[i] = actionList->GetDataAt(i)->Fire(p...);
+			}
+			return retArray;
+		}
+
+	}
+
+	/// <summary>
+	/// Fires every function in subscribe order
+	/// </summary>
+	/// <param name="...p">An array of parameters for the functions to use</param>
+	/// <returns>An array of the returned value of the functions</returns>
+	Return* FireAll(Params... p[])
+	{
+		if (typeid(Return) == typeid(void))
+		{
+			int length = actionList->Count();
+			for (int i = 0; i < length; i++)
+			{
+				actionList->GetDataAt(i)->Fire(p[i]...);
+			}
+			return NULL;
+		}
+		else
+		{
+			int length = actionList->Count();
+			Return* retArray = new Return[length];
+			for (int i = 0; i < length; i++)
+			{
+				retArray[i] = actionList->GetDataAt(i)->Fire(p[i]...);
+			}
+			return retArray;
+		}
+	}
+
+	bool UnSubscribe(Return(*function)(Params...)) // TODO
+	{
+		
+	}
+
+	/// <summary>
+	/// Subscribes a function to the delegate
+	/// </summary>
+	/// <param name="function">The pointer to the function</param>
+	/// <returns>Whether the subscription was sucessful</returns>
+	bool Subscribe(Return(*function)(Params...))
+	{
+		DDelegateAction<Return, Params...> tmp = new DDelegateAction<Return, Params...>(function);
+		if (tmp != nullptr)
+		{
+			actionList->PushAfter(tmp);
+			return true;
+		}
+		else return false;
+	}
+
+
+	DLinkedList<DDelegateAction<Return, Params...>*>* actionList;
+
+
+
+private:
+};
+
+
+
+
+bool Test(bool asd, int dsa)
+{
+	say "Param1: " << asd;
+	creturn;
+	say "Param2: " << dsa;
+	creturn;
+	return true;
+}
+
+
 int main()
 {
-	say ConvertToInt("-654");
-	creturn;
-	say ConvertToInt("654asd");
-	creturn;
-	say ConvertToInt("-654asd");
-	creturn;
-	say ConvertToInt("642654");
-	creturn;
-	say ConvertToInt("asd");
-	creturn;
+	_CrtDumpMemoryLeaks();
+	{
+
+		DDelegate<bool, bool, int>* a = new DDelegate<bool, bool, int>();
+		a->Subscribe(&Test);
+		a->Subscribe(&Test);
+		//a->actionList->PushAfter(new DDelegateAction<bool, bool, int>(&Test));
+		bool* tmp;
+		bool arg1[] = { true, false };
+		int arg2[] = { 0, 42 };
+		tmp = a->FireAll(arg1, arg2);
+		free(tmp);
+		//DLinkedList<int>* asd = new DLinkedList<int>;
+		//for (int i = 0; i < 1000; i++)
+		//{
+		//	asd->PushBefore(i); // (asd->Count() == 0) ? 0 : rand() % asd->Count()
+		//}
+		//say "full"; 
+		//asd->operator[](32)->data;
+		//creturn;
+		////for (int i = 0; i < asd->Count(); i++)
+		////{
+		////	asd->SeekToIndex(i);
+		////	say asd->currentElement->data;
+		////	creturn;
+		////}
+		//for (int i = 0; i < asd->Count() / 2; i++)
+		//{
+		//	asd->Swap(i, asd->Count() - 1 - i);
+		//}
+		//say "reversed";
+		//creturn;
+		//for (int i = 0; i < asd->Count(); i++)
+		//{
+		//	asd->SeekToIndex(i);
+		//	say asd->currentElement->data;
+		//	creturn;
+		//}
+		//delete asd;
+		delete a; // Persze, itt keresem 45 percen keresztül, hogy honnan jön a 8 byte leak
+		// aztán delete helyett csak a destruktort hívtam meg...
+		// Tanúlság? éjfél elõtt foglalkozzak ilyenekkel
+	}
 	int leaks = _CrtDumpMemoryLeaks();
 	say "Leaks: " << leaks;
 }
-
 
 /// <summary>
 /// Sorts array elements using Quicksort.
